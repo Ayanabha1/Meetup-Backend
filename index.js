25e3f8b7263cdddfe24de7cdce887930d19daf3d
Ayanabha Misra <misrarimbo@gmail.com>
2023-07-24 16:26:35 +0530

    Frontend build loaded

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 require("dotenv/config");
 const authRoute = require("./Routes/authRoute");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
 
 // DB connection
 
@@ -24,6 +26,9 @@ const connectDB = () => {
 
 // Routes and middlewares
 app.use("/api/v1/auth", authRoute);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 // Listening to the server
 server.listen(PORT, () => {
