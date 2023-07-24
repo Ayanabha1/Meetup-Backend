f81f50c30db8929192dd6ea12f3b42ea46d7739a
Ayanabha Misra <misrarimbo@gmail.com>
2023-07-24 18:23:07 +0530

    New version of build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 require("dotenv/config");
 const authRoute = require("./Routes/authRoute");
@@ -27,10 +27,7 @@ const connectDB = () => {
 // Routes and middlewares
 app.use("/api/v1/auth", authRoute);
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"), {
-    VITE_AGORA_APP_ID: process.env.AGORA_APP_ID,
-    VITE_API_URL: `http:localhost:${PORT}/api/v1`,
-  });
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // Listening to the server
